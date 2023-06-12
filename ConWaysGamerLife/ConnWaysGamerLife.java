bd24f11dfe40edaf6503d7ffdb7ddc81cc02730d
EmmettPetty <pettyem@whs.school.nz>
2023-06-12 22:14:06 +1200

    Refined the basic grid. made the user experience a little simpiler. Still untested i forgot. Split isnt working for unkown reasons. Also need to figure out CONST.

--- ConWaysGamerLife/ConnWaysGamerLife.java
+++ ConWaysGamerLife/ConnWaysGamerLife.java
@@ -5,24 +5,22 @@ public class ConnWaysGamerLife
     int GRIDSIZE = 25;// wanna make this a constant
     Scanner keyboard;
     boolean ConWayActive = true;
-        public ConnWaysGamerLife()
+    public ConnWaysGamerLife()
     {
         int[][] Grid = new int[GRIDSIZE][GRIDSIZE]; 
         for (int x=0;x<GRIDSIZE;x++) 
         for (int y=0;y<GRIDSIZE;y++) {
                 Grid[x][y] = 1;
-
             }
-        while (ConWayActive) {
-        System.out.println("Give me a position between 1 & 25"); //Instructions
+        
+        System.out.println("Give me a 2 positions between 1 & 25 seperated by a comma"); //Instructions
         keyboard = new Scanner(System.in);
-        int N1=(keyboard.nextInt())-1;
-        System.out.println("Give me a position between 1 & 25"); //Instructions    
-        keyboard = new Scanner(System.in);
-        int N2=(keyboard.nextInt())-1;
+        String[] nums=keyboard.split(","); //Split doesnt work )=
+        int N1 = Integer.parseInt(nums[0]);
+        int N2 = Integer.parseInt(nums[1]);
         if (Grid[N1][N2] == 4) Grid[N1][N2] = 3;
         else Grid[N1][N2] = 2;
         System.out.print('\u000C');
-        }
+        
     }
 }
