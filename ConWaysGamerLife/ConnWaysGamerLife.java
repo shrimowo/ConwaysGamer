bc36ad6c3a8efd3b61b9460a0130f0fce1b27a72
pettyem <pettyem@whs.school.nz>
2023-06-13 12:50:18 +1200

    Added Grid creator Added starting screen Fixed split(); issue Along with some minor changes

--- ConWaysGamerLife/ConnWaysGamerLife.java
+++ ConWaysGamerLife/ConnWaysGamerLife.java
@@ -4,21 +4,39 @@ public class ConnWaysGamerLife
     // instance variables - replace the example below with your own
     int GRIDSIZE = 25;// wanna make this a constant
     Scanner keyboard;
-    boolean ConWayActive = true;
+    boolean StartScreen = true;
+    boolean GridGenerating = true;
     public ConnWaysGamerLife()
     {
         int[][] Grid = new int[GRIDSIZE][GRIDSIZE]; 
-        for (int x=0;x<GRIDSIZE;x++) 
+        for (int x=0;x<GRIDSIZE;x++) //Sets the grid as off by default
         for (int y=0;y<GRIDSIZE;y++) {
                 Grid[x][y] = 1;
+        }
+        while (StartScreen) {
+            System.out.println("Welcome To Conways Game Of Life"); //Instructions
+            System.out.println("Type 'start' To Begin The Game");
+            System.out.println("Type 'guide' To Be Given Instructions");
+            keyboard = new Scanner(System.in);
+            String answer=keyboard.nextLine();
+            if (answer
+        }
+        for (int x=0;x<GRIDSIZE;x++) {
+            for (int y=0;y<GRIDSIZE;y++) {
+                if (Grid[x][y] == 1) System.out.print("o ");
+                else if (Grid[x][y] == 2) System.out.print("x ");
             }
-        
+            System.out.println("");
+                    }
         System.out.println("Give me a 2 positions between 1 & 25 seperated by a comma"); //Instructions
         keyboard = new Scanner(System.in);
-        String[] nums=keyboard.split(","); //Split doesnt work )=
+        String[] nums=keyboard.nextLine().split(",");
         int N1 = Integer.parseInt(nums[0]);
         int N2 = Integer.parseInt(nums[1]);
+        Grid[N1][N2] = 2;
         System.out.print('\u000C');
-        //Type 1 if you are SILLY
-    }
-}
+        System.out.println("Grid done");
+        GridGenerating = false;
+                }
+        }
+
