dda9a3f703b0e6446ba85e6a3b85417247b953ae
pettyem <pettyem@whs.school.nz>
2023-06-13 15:17:50 +1200

    Added a generations input Fixed the on/off system & grid creator so it can run more than once Almost finished starting screen, exept for the description of the game (because i was lazy) Also added a method to stop the grid addition

--- ConWaysGamerLife/ConnWaysGamerLife.java
+++ ConWaysGamerLife/ConnWaysGamerLife.java
@@ -9,34 +9,51 @@ public class ConnWaysGamerLife
     public ConnWaysGamerLife()
     {
         int[][] Grid = new int[GRIDSIZE][GRIDSIZE]; 
+        System.out.print('\u000C');
         for (int x=0;x<GRIDSIZE;x++) //Sets the grid as off by default
         for (int y=0;y<GRIDSIZE;y++) {
                 Grid[x][y] = 1;
         }
+        System.out.println("Welcome To Conways Game Of Life"); //Instructions
+        System.out.println("Type 'guide' To Be Given Instructions");
         while (StartScreen) {
-            System.out.println("Welcome To Conways Game Of Life"); //Instructions
             System.out.println("Type 'start' To Begin The Game");
-            System.out.println("Type 'guide' To Be Given Instructions");
             keyboard = new Scanner(System.in);
             String answer=keyboard.nextLine();
-            if (answer
-        }
-        for (int x=0;x<GRIDSIZE;x++) {
-            for (int y=0;y<GRIDSIZE;y++) {
-                if (Grid[x][y] == 1) System.out.print("o ");
-                else if (Grid[x][y] == 2) System.out.print("x ");
+            answer = answer.toLowerCase();
+            System.out.println(answer);
+            if (answer.equals("start")) StartScreen = false;
+            else if (answer.equals("guide")) {
+                System.out.println("I'll do this l8ter");
             }
-            System.out.println("");
-                    }
-        System.out.println("Give me a 2 positions between 1 & 25 seperated by a comma"); //Instructions
-        keyboard = new Scanner(System.in);
-        String[] nums=keyboard.nextLine().split(",");
-        int N1 = Integer.parseInt(nums[0]);
-        int N2 = Integer.parseInt(nums[1]);
-        Grid[N1][N2] = 2;
-        System.out.print('\u000C');
-        System.out.println("Grid done");
-        GridGenerating = false;
-                }
+            else System.out.println("Sorry your input was invalid");
         }
+        while (GridGenerating) {
+            System.out.print('\u000C');
+            for (int x=0;x<GRIDSIZE;x++) {
+                for (int y=0;y<GRIDSIZE;y++) {
+                    if (Grid[x][y] == 1) System.out.print("o ");
+                    else if (Grid[x][y] == 2) System.out.print("x ");
+                }
+                System.out.println("");
+            }
+            System.out.println("Give me a 2 positions between 1 & 25 seperated by a comma"); //Instructions
+            System.out.println("If you are done type 'done'"); //Instructions
+            keyboard = new Scanner(System.in);
+            String doneCheck = keyboard.nextLine().toLowerCase();
+            if (doneCheck.equals("done")) GridGenerating = false;
+                else {
+                String[] nums=keyboard.nextLine().split(",");
+                int N1 = Integer.parseInt(nums[0]);
+                int N2 = Integer.parseInt(nums[1]);
+                Grid[N1][N2] = 2;
+                System.out.print('\u000C');
+            }
+            System.out.print('\u000C');
+            System.out.println("How many generations do you want the game to run for?"); //Instructions
+            keyboard = new Scanner(System.in);
+            int generations = Integer.parseInt(keyboard.nextLine());
+        }
+    }
+}
 
