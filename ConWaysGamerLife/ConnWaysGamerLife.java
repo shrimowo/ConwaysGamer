584e3ed5cc0382749f3b1b3a6001e044ed6a9728
pettyem <pettyem@whs.school.nz>
2023-06-22 11:58:34 +1200

    Gotta figure out how to make it only use 1 line (aka not treat it as 2 different inputs I made some minor changes

--- ConWaysGamerLife/ConnWaysGamerLife.java
+++ ConWaysGamerLife/ConnWaysGamerLife.java
@@ -28,8 +28,8 @@ public class ConnWaysGamerLife
             }
             else System.out.println("Sorry your input was invalid");
         }
+        System.out.print('\u000C');
         while (GridGenerating) {
-            System.out.print('\u000C');
             for (int x=0;x<GRIDSIZE;x++) {
                 for (int y=0;y<GRIDSIZE;y++) {
                     if (Grid[x][y] == 1) System.out.print("o ");
@@ -44,16 +44,16 @@ public class ConnWaysGamerLife
             if (doneCheck.equals("done")) GridGenerating = false;
                 else {
                 String[] nums=keyboard.nextLine().split(",");
-                int N1 = Integer.parseInt(nums[0]);
-                int N2 = Integer.parseInt(nums[1]);
+                int N1 = Integer.parseInt(nums[0])-1;
+                int N2 = Integer.parseInt(nums[1])-1;
                 Grid[N1][N2] = 2;
                 System.out.print('\u000C');
             }
-            System.out.print('\u000C');
-            System.out.println("How many generations do you want the game to run for?"); //Instructions
-            keyboard = new Scanner(System.in);
-            int generations = Integer.parseInt(keyboard.nextLine());
         }
+        System.out.print('\u000C');
+        System.out.println("How many generations do you want the game to run for?"); //Instructions
+        keyboard = new Scanner(System.in);
+        int generations = Integer.parseInt(keyboard.nextLine());
     }
 }
 
